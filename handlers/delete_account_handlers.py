a5232903504bb5f82370bc9bfa1930eeda3f8623
Binobinos <binobinos.dev@gmail.com>
2025-06-07 16:34:37 +0300

    Финальный релиз. Изменения

--- handlers/delete_account_handlers.py
+++ handlers/delete_account_handlers.py
@@ -6,15 +6,16 @@ from config import Query, bot, conn, callback_query
 @bot.on(Query(data=lambda event: event.decode().startswith(f"delete_account_")))
 async def handle_user_input(event: callback_query):
     user_id: int = int(event.data.decode().strip().split("_")[2])
-    with conn.cursor() as cursor:
-        cursor.execute("SELECT session_string FROM sessions WHERE user_id = ?", (user_id,))
-        user = cursor.fetchone()
+    cursor = conn.cursor()
+    cursor.execute("SELECT session_string FROM sessions WHERE user_id = ?", (user_id,))
+    user = cursor.fetchone()
 
-        if user:
-            cursor.execute("DELETE FROM sessions WHERE user_id = ?", (user_id,))
-            conn.commit()
-            logging.info(f"✅ Аккаунт  {user_id} успешно удален.")
-            await event.respond(f"✅ Аккаунт id={user_id} успешно удален.")
-        else:
-            logging.warning(f"Аккаунт id={user} не найден")
-            await event.respond("⚠ Этот аккаунт не найден в базе данных.")
+    if user:
+        cursor.execute("DELETE FROM sessions WHERE user_id = ?", (user_id,))
+        conn.commit()
+        logging.info(f"✅ Аккаунт  {user_id} успешно удален.")
+        await event.respond(f"✅ Аккаунт id={user_id} успешно удален.")
+    else:
+        logging.warning(f"Аккаунт id={user} не найден")
+        await event.respond("⚠ Этот аккаунт не найден в базе данных.")
+    cursor.close()
